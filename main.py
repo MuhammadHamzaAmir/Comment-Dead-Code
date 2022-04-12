85c0979a11eeee0b1f7ec274e1cdf7dff7ec7afb
MuhammadHamzaAmir <43249821+MuhammadHamzaAmir@users.noreply.github.com>
2022-04-12 18:03:00 +0500

    Methods name changed from dead to unused. Output is displayed to the terminal

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import os
 import re
 
 
-def get_dead_methods(class_file_path:str,usage_file_path:str) -> str:
+def get_unused_methods(class_file_path:str,usage_file_path:str) -> str:
     """
-    The get_dead_methods function takes a class file path and usage file path as input. 
+    The get_unused_methods function takes a class file path and usage file path as input. 
     It returns the names of all methods that are not invoked in the usage file, 
     as well as their return types. The returned value is a string with one method per line, 
     in alphabetical order by method name.
@@ -21,23 +21,23 @@ def get_dead_methods(class_file_path:str,usage_file_path:str) -> str:
 
 
 
-def get_dead_methods_names_lines_numbers(dead_methods:str) -> dict:
+def get_unused_methods_names_lines_numbers(unused_methods:str) -> dict:
     """
-    The get_dead_methods_names_lines_numbers function takes in a string of dead code and returns a dictionary with the line numbers as keys and method names as values.
+    The get_unused_methods_names_lines_numbers function takes in a string of unused code and returns a dictionary with the line numbers as keys and method names as values.
     
-    :param dead_methods:str: Used to Pass the string of dead methods to the function.
-    :return: A dictionary with the lines numbers of the dead code as keys and their method names as values.
+    :param unused_methods:str: Used to Pass the string of unused methods to the function.
+    :return: A dictionary with the lines numbers of the unused code as keys and their method names as values.
     
     :doc-author: Trelent
     """
     dict = {}
-    dead_methods_list = dead_methods.split('\n')
-    dead_methods_list.pop()
+    unused_methods_list = unused_methods.split('\n')
+    unused_methods_list.pop()
 
     num_regex = re.compile(":[0-9]+:")
     method_regex = re.compile("\'.*\'")
 
-    for i in dead_methods_list:
+    for i in unused_methods_list:
 
         if (i.find("method")) != -1:
             method_name = method_regex.search(i)
@@ -47,9 +47,9 @@ def get_dead_methods_names_lines_numbers(dead_methods:str) -> dict:
 
 
 
-def comment_dead_methods(dictionary:dict,file_path:str) -> None:
+def comment_unused_methods(dictionary:dict,file_path:str) -> None:
     """
-    The comment_dead_methods function takes a dictionary of methods and their line numbers in the file,
+    The comment_unused_methods function takes a dictionary of methods and their line numbers in the file,
     and comments out all lines between the method definition and the end of that method.
     
     :param dictionary:dict: Used to Store the line numbers of all methods that are to be commented.
@@ -59,7 +59,7 @@ def comment_dead_methods(dictionary:dict,file_path:str) -> None:
     :doc-author: Trelent
     """
     if len(dictionary)<1:
-        print("No dead methods found")
+        print("\n\t<--- No unused methods found --->\n")
         return None
     
     file = open(file_path,'r')
@@ -71,11 +71,14 @@ def comment_dead_methods(dictionary:dict,file_path:str) -> None:
     indent_check = int(next(iter(dictionary)))
     indent_index = lines[indent_check-1].find('def')
 
-    string_exp = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ._(){[]}0123456789+-*/%&^"
+    string_exp = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ._(){[]}0123456789+-*/%&^#"
+
+    print("<--- Unused Methods --->\n")
 
     for i, (k, v) in enumerate(dictionary.items()):
         current_line_num = int(k)-1
 
+        print(str(i+1)+") "+v+"\n")
         out_method = False
         while(current_line_num < len(lines)):
             current_line = lines[current_line_num]
@@ -107,13 +110,8 @@ def comment_dead_methods(dictionary:dict,file_path:str) -> None:
 
 
 
-import time
-t1 = time.time()
-p = get_dead_methods("mc.py","user.py")
-
-q = get_dead_methods_names_lines_numbers(p)
-comment_dead_methods(q,"mc.py")
-t2 = time.time()
-print(t2-t1,q)
+p = get_unused_methods("class_file.py","usage_file.py")
+q = get_unused_methods_names_lines_numbers(p)
+comment_unused_methods(q, "class_file.py")
 
 
